42a71c863cf79db98d77feda67dea8ced64e4788
Koji Eguchi <koji.eguchi@hotmail.com>
2024-05-19 17:39:39 -0400

    add an email form test

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,5 +4,9 @@ describe("form test", () => {
   });
   it("Test subscribe form", () => {
     cy.contains(/testing forms/i);
+    cy.getDataTest("subscribe-form").find("input").type("example@test.com");
+    cy.contains(/Successfully subbed: example@test.com!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/Successfully subbed: example@test.com!/i).should("exist");
   });
 });
