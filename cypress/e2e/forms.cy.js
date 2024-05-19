c9761ec749985d5c51337caa9e65fa71115451ff
Koji Eguchi <koji.eguchi@hotmail.com>
2024-05-19 17:53:01 -0400

    add an email form validation tests

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,9 +4,25 @@ describe("form test", () => {
   });
   it("Test subscribe form", () => {
     cy.contains(/testing forms/i);
-    cy.getDataTest("subscribe-form").find("input").type("example@test.com");
+    cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
+    cy.get("@subscribe-input").type("example@test.com");
     cy.contains(/Successfully subbed: example@test.com!/i).should("not.exist");
     cy.getDataTest("subscribe-button").click();
     cy.contains(/Successfully subbed: example@test.com!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/Successfully subbed: example@test.com!/i).should("not.exist");
+
+    cy.get("@subscribe-input").type("example@test.io");
+    cy.contains(/invalid email: example@test.io!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/invalid email: example@test.io!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/invalid email: example@test.io!/i).should("not.exist");
+
+    cy.contains(/fail!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/fail!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/fail!/i).should("not.exist");
   });
 });
